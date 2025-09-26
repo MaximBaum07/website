2e9a45837d53f4ca0cc05c3665ce797557d6e4f0
MaximBaum07 <maxim-baumann@outlook.com>
2025-09-26 16:38:11 +0200

    removed particle js onHover when on mobile; added mod overview mobie view

--- mod/mod.js
+++ mod/mod.js
@@ -1,11 +1,22 @@
-particlesJS.load(
-  "particles-js",
-  "../assets/particle_js/particlesjs-config.json",
-  function () {
-    console.log("particles.js config loaded");
-  }
-);
+var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
 
+if (width <= 600) {
+  particlesJS.load(
+    "particles-js",
+    "../assets/particle_js/particlesjs-mobile-config.json",
+    function () {
+      console.log("particles.js config loaded");
+    }
+  );
+} else {
+  particlesJS.load(
+    "particles-js",
+    "../assets/particle_js/particlesjs-config.json",
+    function () {
+      console.log("particles.js config loaded");
+    }
+  );
+}
 document.getElementById("asciiDiv").addEventListener("click", function () {
   window.location.href = "/";
 });
